e8fa48bb323127be80673b058dafdbf784457bd7
Matt Proud <laidbackware.git@gmail.com>
2024-10-08 13:42:33 +0100

    add spring boot

---
+++ springboot/src/test/java/com/rabbitmq/cftutorial/rabbitmq_spring/RabbitmqSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rabbitmq.cftutorial.rabbitmq_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RabbitmqSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
